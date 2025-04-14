99d39e2424ce13a23cff19d9ac4ad401057fd77a
William Oliveira <w.oliveira542@gmail.com>
2025-04-14 17:00:31 +0200

    start project

---
+++ scripts/player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+var speed: float = 300.0
+var viewport_size
+
+func _ready() -> void:
+	viewport_size = get_viewport_rect().size
+
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+	
+	move_and_slide()
+	
+	var margin = 20
+	if global_position.x  > viewport_size.x + margin:
+		global_position.x = -margin
+
+	if global_position.x < -margin:
+		global_position.x = viewport_size.x + margin
