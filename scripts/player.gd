bf2c259be5b0c7fb2a316f2905949efbba5d53ab
William Oliveira <w.oliveira542@gmail.com>
2025-04-17 10:54:16 +0200

    add accelerometer

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,10 +8,16 @@ var gravity: float = 15.0
 var max_fall_velocity = 1000.0
 var jump_velocity = -800
 
+var accelerometer_speed = 130.0
+var use_accelerometer = false
+
 var viewport_size
 
 func _ready() -> void:
 	viewport_size = get_viewport_rect().size
+	var os_name = OS.get_name()
+	if os_name == "Android" || os_name == "iOS" || os_name == "Web":
+		use_accelerometer = true
 
 func _process(_delta: float) -> void:
 	if velocity.y > 0:
@@ -27,11 +33,15 @@ func _physics_process(_delta: float) -> void:
 	if velocity.y > max_fall_velocity:
 		velocity.y = max_fall_velocity
 	
-	var direction = Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * speed
+	if use_accelerometer:
+		var direction = Input.get_accelerometer()
+		velocity.x = direction.x * accelerometer_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+		var direction = Input.get_axis("move_left", "move_right")
+		if direction:
+			velocity.x = direction * speed
+		else:
+			velocity.x = move_toward(velocity.x, 0, speed)
 	
 	move_and_slide()
 	
